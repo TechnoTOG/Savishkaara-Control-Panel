6fdca23928087c5cfefc3a244855bba837315825
TechnoTOG <xaviererick879@gmail.com>
2025-01-26 18:33:30 +0530

    changed default color and adjusted icons

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -15,23 +15,30 @@ const Sidebar = ({ isMinimized }) => {
       sx={{
         width: isMinimized ? 80 : 250,
         height: "100vh",
-        backgroundColor: "#0d1b33",
+        backgroundColor: "#101d44",
         color: "#fff",
         paddingTop: "20px",
         transition: "width 0.3s ease",
         position: "fixed",
         overflowX: "hidden",
-        
       }}
     >
-      <Box textAlign="center" mb={4} display="flex" alignItems="center" justifyContent={isMinimized ? "center" : "space-around"}>
-        <img src="/icons/logo.png" alt="logo" width={isMinimized ? "40" : "80"} />
+      <Box 
+        textAlign="center" 
+        mb={4} 
+        display="flex" 
+        alignItems="center" 
+        justifyContent={isMinimized ? "center" : "space-around"} 
+        paddingX={isMinimized ? 0 : 2}
+      >
+        <img src="/icons/saavishkaara_logo_new.png" alt="logo" width={isMinimized ? "40" : "80"} />
+        {!isMinimized && <span style={{ fontSize: "1.2rem", fontWeight: "bold", paddingRight: "5px" }}>Savishkaara</span>}
       </Box>
 
       <List component="nav">
         <Tooltip title="Dashboard" placement="right" disableHoverListener={!isMinimized}>
-          <ListItemButton sx={{ color: "#fff" }}>
-            <ListItemIcon>
+          <ListItemButton sx={{ color: "#fff", justifyContent: isMinimized ? "left" : "flex-start" }}>
+            <ListItemIcon sx={{paddingLeft: isMinimized ? "10px" : "0px"}}>
               <img src="/Icons/home_24dp_EFEFEF_FILL0_wght400_GRAD0_opsz24.svg" alt="dashboard" width="24" />
             </ListItemIcon>
             {!isMinimized && <ListItemText primary="Dashboard" />}
@@ -39,8 +46,11 @@ const Sidebar = ({ isMinimized }) => {
         </Tooltip>
 
         <Tooltip title="Events" placement="right" disableHoverListener={!isMinimized}>
-          <ListItemButton onClick={handleEventsClick} sx={{ color: "#fff" }}>
-            <ListItemIcon>
+          <ListItemButton 
+            onClick={handleEventsClick} 
+            sx={{ color: "#fff", justifyContent: isMinimized ? "left" : "flex-start" }}
+          >
+            <ListItemIcon sx={{paddingLeft: isMinimized ? "10px" : "0px"}}>
               <img src="/Icons/theater_comedy_24dp_EFEFEF_FILL0_wght400_GRAD0_opsz24.svg" alt="events" width="24" />
             </ListItemIcon>
             {!isMinimized && <ListItemText primary="Events" />}
@@ -49,15 +59,15 @@ const Sidebar = ({ isMinimized }) => {
         </Tooltip>
         <Collapse in={openEvents && !isMinimized} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
-            <ListItemButton sx={{ pl: 4, color: "#aaa" }}>
+            <ListItemButton sx={{ pl: 6, color: "#aaa" }}>
               <ListItemText primary="Add Event" />
             </ListItemButton>
           </List>
         </Collapse>
 
         <Tooltip title="Samridhi" placement="right" disableHoverListener={!isMinimized}>
-          <ListItemButton sx={{ color: "#fff" }}>
-            <ListItemIcon>
+          <ListItemButton sx={{ color: "#fff", justifyContent: isMinimized ? "left" : "flex-start" }}>
+            <ListItemIcon sx={{paddingLeft: isMinimized ? "10px" : "0px"}}>
               <img src="/Icons/fastfood_24dp_EFEFEF_FILL0_wght400_GRAD0_opsz24.svg" alt="samridhi" width="24" />
             </ListItemIcon>
             {!isMinimized && <ListItemText primary="Samridhi" />}
@@ -65,8 +75,11 @@ const Sidebar = ({ isMinimized }) => {
         </Tooltip>
 
         <Tooltip title="Users" placement="right" disableHoverListener={!isMinimized}>
-          <ListItemButton onClick={handleUsersClick} sx={{ color: "#fff" }}>
-            <ListItemIcon>
+          <ListItemButton 
+            onClick={handleUsersClick} 
+            sx={{ color: "#fff", justifyContent: isMinimized ? "left" : "flex-start" }}
+          >
+            <ListItemIcon sx={{paddingLeft: isMinimized ? "10px" : "0px"}}>
               <img src="/Icons/manage_accounts_24dp_EFEFEF_FILL0_wght400_GRAD0_opsz24.svg" alt="users" width="24" />
             </ListItemIcon>
             {!isMinimized && <ListItemText primary="Users" />}
@@ -75,15 +88,15 @@ const Sidebar = ({ isMinimized }) => {
         </Tooltip>
         <Collapse in={openUsers && !isMinimized} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
-            <ListItemButton sx={{ pl: 4, color: "#aaa" }}>
+            <ListItemButton sx={{ pl: 6, color: "#aaa" }}>
               <ListItemText primary="Add User" />
             </ListItemButton>
           </List>
         </Collapse>
 
         <Tooltip title="Server" placement="right" disableHoverListener={!isMinimized}>
-          <ListItemButton sx={{ color: "#fff" }}>
-            <ListItemIcon>
+          <ListItemButton sx={{ color: "#fff", justifyContent: isMinimized ? "left" : "flex-start" }}>
+            <ListItemIcon sx={{paddingLeft: isMinimized ? "10px" : "0px"}}>
               <img src="/Icons/host_24dp_EFEFEF_FILL0_wght400_GRAD0_opsz24.svg" alt="server" width="24" />
             </ListItemIcon>
             {!isMinimized && <ListItemText primary="Server" />}
@@ -95,4 +108,3 @@ const Sidebar = ({ isMinimized }) => {
 };
 
 export default Sidebar;
-
