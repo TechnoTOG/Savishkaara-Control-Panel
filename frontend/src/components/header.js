d030e9ef8a4edfc096ecdd8031eff43744dfd98c
Erick Xavier <xaviererick879@gmail.com>
2025-01-30 11:00:55 +0530

    light icons modifications

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -9,7 +9,7 @@ const Navbar = ({ toggleSidebar }) => {
     <div className="navbar">
       <div className="navbar-left">
         <img 
-          src="\Icons\menu_24dp_EFEFEF_FILL0_wght400_GRAD0_opsz24.svg" 
+          src="\Icons\menu_24dp_44464b_FILL0_wght400_GRAD0_opsz24.svg" 
           alt="Menu" 
           className="hamburger-menu" 
           onClick={toggleSidebar} 
@@ -18,8 +18,8 @@ const Navbar = ({ toggleSidebar }) => {
       </div>
 
       <div className="navbar-icons">
-        <img src="\Icons\dark_mode_24dp_EFEFEF_FILL0_wght400_GRAD0_opsz24.svg" alt="Dark Mode" />
-        <img src="Icons\notifications_24dp_EFEFEF_FILL0_wght400_GRAD0_opsz24.svg" alt="Notifications" />
+        <img src="\Icons\dark_mode_24dp_44464b_FILL0_wght400_GRAD0_opsz24.svg" alt="Dark Mode" />
+        <img src="Icons\notifications_24dp_44464b_FILL0_wght400_GRAD0_opsz24.svg" alt="Notifications" />
       </div>
 
       <div className="navbar-profile">
