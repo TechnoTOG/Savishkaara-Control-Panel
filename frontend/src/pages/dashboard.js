87366997f9c4e955cace42cdcd7d89a0bcaae2e3
Meenakshi <meenuptr2002@gmail.com>
2025-02-25 21:24:05 +0530

    dashboard layout adjusted

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -57,55 +57,49 @@ const Dashboard = () => {
         </Typography>
       )}
 
-      <div style={{ marginLeft: "20px", width: "90%" }}>
+      <div style={{ marginLeft: "20px", width: "97%" }}>
         <Grid container spacing={3}>
           {/* First Column */}
           <Grid item xs={12} md={3}>
             <Grid container spacing={3} direction="column">
               <Grid item>
-                <MetricCard title="Total Registration" height="200px" value={5000} />
+                <MetricCard title="Total Registration" height="235px" value={5000} />
               </Grid>
               <Grid item>
-                <MetricCard title="Active Users" height="200px" value={2456} />
+                <MetricCard title="Active Users" height="240px" value={2456} />
               </Grid>
             </Grid>
           </Grid>
 
           {/* Second Column */}
           <Grid item xs={12} md={5}>
-            <Grid container spacing={3} direction="column">
-              {/* First Row in Second Column (2 columns) */}
-              <Grid item>
-                <Grid container spacing={3}>
-                  <Grid item xs={6}>
-                    <MetricCard title="Total Revenue" height="100px" value={150000} />
-                  </Grid>
-                  <Grid item xs={6}>
-                    <MetricCard title="Total Participation" height="100px" value={3000} />
-                  </Grid>
-                </Grid>
+            <Grid container spacing={3}>
+              {/* Total Revenue */}
+              <Grid item xs={12} sm={6}>
+                <MetricCard title="Total Revenue" height="100px" value={150000} />
               </Grid>
-              {/* Second Row in Second Column (2 columns) */}
-              <Grid item>
-                <Grid container spacing={3}>
-                  <Grid item xs={6}>
-                    <VisualizationCard title="Revenue and Event Day by Day" chartType="line" height="130px" />
-                  </Grid>
-                  <Grid item xs={6}>
-                    <VisualizationCard title="Alerts & Notifications" chartType="bar" height="130px" />
-                  </Grid>
-                </Grid>
+              {/* Total Participation */}
+              <Grid item xs={12} sm={6}>
+                <MetricCard title="Total Participation" height="100px" value={3000} />
               </Grid>
-              {/* Third Row in Second Column (1 column) */}
+              {/* Revenue and Event Day by Day */}
+              <Grid item xs={12} sm={6}>
+                <VisualizationCard title="Revenue and Event Day by Day" chartType="line" height="130px" />
+              </Grid>
+              {/* Alerts & Notifications */}
+              <Grid item xs={12} sm={6}>
+                <VisualizationCard title="Alerts & Notifications" chartType="bar" height="130px" />
+              </Grid>
+              {/* Top Events */}
               <Grid item xs={12}>
-                <VisualizationCard title="Top Events" chartType="pie" height="180px" />
+                <VisualizationCard title="Top Events" chartType="pie" height="260px" />
               </Grid>
             </Grid>
           </Grid>
 
           {/* Third Column */}
           <Grid item xs={12} md={4}>
-            <VisualizationCard title="Event Update" chartType="area" height="500px" />
+            <VisualizationCard title="Event Update" chartType="area" height="580px" />
           </Grid>
         </Grid>
       </div>
