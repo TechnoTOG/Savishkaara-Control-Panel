c8b340082ee94601b021ac13a53b2813da46c4cb
TechnoTOG <xaviererick879@gmail.com>
2025-03-27 15:12:25 +0530

    Model for Events collection

---
+++ backend/models/events.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  fee: { type: Number, required: true },
+  date_time: { type: Date, required: true }, // Combined date and time
+  form_link: { type: String, required: true },
+  excel_link: { type: String, required: true },
+  coordinator1: { type: String, required: true },
+  coordinator2: { type: String, required: true },
+  faculty_coor1: { type: String, required: true },
+  faculty_coor2: { type: String, required: true },
+  status: { type: String, required: true },
+  venue: { type: String, required: true }
+}, { collection: "events"});
+
+const Event = mongoose.model("Event", eventSchema);
+module.exports = Event;
--- backend/routes/event.js
+++ backend/routes/event.js
@@ -2,15 +2,22 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-const Event = require('../models/event_registration');
+// Import the User model 
+const Events = require('../models/events');
+const Event_reg = require('../models/event_registration');
 
 router.get("/events-count", async (req, res) => {
   try {
-    const count = await Event.countDocuments();
+    const count = await Event_reg.countDocuments();
     res.status(200).json({ count: count });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+});
+
+router.post('/addEvent', (req, res) => {
+    const { name, venue,  dateAndTime, fee, coor1, coor2, facoor1, facoor2, flinik, elink } = req.body;
+
 });
 
 module.exports = router;
